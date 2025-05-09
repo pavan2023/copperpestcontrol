3ab41332ddaeb436063951bb66d5801df00565c9
pavan2023 <pavanchavan2023@gmail.com>
2025-05-09 14:06:12 +0530

    tailwind links fixed

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ window.onload = function () {
     if (logo) logo.remove();
   }
 };
+
+window.onload = function () {
+  var splineElement = document.querySelectorAll("spline-viewer");
+
+  for (let pas = 0; pas < splineElement.length; pas++) {
+    var shadowRoot = splineElement[pas].shadowRoot;
+    shadowRoot.querySelector("ahashtag#logo").remove();
+  }
+};
 // Spline code End
 
 // Component reusable code start
